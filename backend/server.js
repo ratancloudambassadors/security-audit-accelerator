011802716d60dc0990f916fa6cfa26f77cbbabfd
pradipsingh-art <pradip.singh@cloudambassadors.com>
2026-04-08 12:16:10 +0530

    more feature

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,8 @@ app.use((req, res, next) => {
   }
   next();
 });
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
